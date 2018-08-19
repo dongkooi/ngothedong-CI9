89ce032762d5f829bdf8e3c94ceb4fa59debb15b
Ngo The Dong <ngothedong97@gmail.com>
2018-08-19 14:44:03 +0700

    Btvn

--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -12,17 +12,14 @@ import java.util.Random;
 public class GameCanvas extends JPanel {
     Image background;
 
-//    int playerX = 268;
-//    int playerY = 660;
-
-    int count;
+//    int count;
     int enemySpawnCount;
 
     ArrayList<PlayerBullet> bullets;
     ArrayList<Enemy> enemies;
 
 
-    boolean shootlock = false;
+//    boolean shootlock = false;
 
     BufferedImage backBuffer;
     Graphics backBufferGraphic;
@@ -36,13 +33,7 @@ public class GameCanvas extends JPanel {
         inputManager = new InputManager();
         bullets = new ArrayList<>();
         PlayerBullet b1 = new PlayerBullet(300, 700);
-//        b1.playerX = 300;
-//        b1.playerY = 700;
-//
         PlayerBullet b2 = new PlayerBullet(600, 600);
-//        b2.playerX = 300;
-//        b2.playerY = 600;
-
         enemies = new ArrayList<>();
 //        Enemy e1 = new Enemy();
 //        e1.playerX = 150;
@@ -60,17 +51,9 @@ public class GameCanvas extends JPanel {
         player.inputManager = inputManager;
         background = ImageUtil.load("images/background/background.png");
 
-//        b1.image = ImageUtil.load("images/bullet/player/mb69bullet1.png");
-//        b2.image = ImageUtil.load("images/bullet/player/mb69bullet1.png");
-//        e1.image = ImageUtil.load("images/enemy/bacteria/bacteria1.png");
-//        e2.image = ImageUtil.load("images/enemy/bacteria/bacteria1.png");
-//        e3.image = ImageUtil.load("images/enemy/bacteria/bacteria1.png");
-//
 //        bullets.add(b1);
 //        bullets.add(b2);
-//        enemies.add(e1);
-//        enemies.add(e2);
-//        enemies.add(e3);
+
 
         backBuffer = new BufferedImage(600, 800, BufferedImage.TYPE_INT_ARGB);
         backBufferGraphic = backBuffer.getGraphics();
@@ -86,7 +69,7 @@ public class GameCanvas extends JPanel {
 
 
     void run() {
-        player.run();
+        player.run(bullets);
         for (PlayerBullet b : bullets) {
             b.run();
         }
@@ -103,40 +86,30 @@ public class GameCanvas extends JPanel {
         }
 
 
-        if (inputManager.xPressed && !shootlock) {
-            PlayerBullet newb = new PlayerBullet(player.x, player.y);
-//            newb.x = playerX;
-//            newb.y = playerY;
-//            try {
-//                newb.image = ImageIO.read(new File("images/bullet/player/mb69bullet1.png"));
-//            } catch (IOException e) {
-//                e.printStackTrace();
+//        if (inputManager.xPressed && !shootlock) {
+//            PlayerBullet newb = new PlayerBullet(player.x, player.y);
+//            bullets.add(newb);
+//            shootlock = true;
+//
+//        }
+//        if (shootlock) {
+//            count++;
+//            if (count > 15) {
+//                shootlock = false;
+//                count = 0;
 //            }
-            bullets.add(newb);
-            shootlock = true;
-
-        }
-        if (shootlock) {
-            count++;
-            if (count > 15) {
-                shootlock = false;
-                count = 0;
-            }
-        }
+//        }
 
     }
 
 
     void render() {
         backBufferGraphic.drawImage(background, 0, 0, null);
-//        backBufferGraphic.drawImage(player.image, player.x, player.y, null);
         player.render(backBufferGraphic);
         for (PlayerBullet b : bullets) {
-//            backBufferGraphic.drawImage(b.image, b.x, b.y, null);
             b.render(backBufferGraphic);
         }
         for (Enemy e : enemies) {
-//            backBufferGraphic.drawImage(e.image, e.x, e.y, null);
             e.render(backBufferGraphic);
 
         }
--- src/Player.java
+++ src/Player.java
@@ -1,11 +1,14 @@
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Player {
     int x;
     int y;
+    int count;
     Image image;
     InputManager inputManager;
+    boolean shootlock = false;
 
     public Player(int x, int y) {
         this.x = x;
@@ -18,7 +21,7 @@ public class Player {
         g.drawImage(this.image, this.x, this.y, null);
     }
 
-    void run() {
+    void run(ArrayList<PlayerBullet> bullets) {
         if (inputManager.rightPressed) {
             this.x += 5;
         }
@@ -30,6 +33,23 @@ public class Player {
         }
         if (inputManager.downPressed) {
             this.y += 5;
+        }
+
+        if (inputManager.xPressed && !shootlock) {
+            PlayerBullet newb = new PlayerBullet(this.x, this.y);
+            bullets.add(newb);
+            shootlock = true;
+
+        }
+        for (PlayerBullet b : bullets) {
+            b.y -= 5;
+        }
+        if (shootlock) {
+            count++;
+            if (count > 15) {
+                shootlock = false;
+                count = 0;
+            }
         }
     }
 }
